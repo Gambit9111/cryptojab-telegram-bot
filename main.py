7b203790709f41b1ea02818e715cd503834e1248
Gambit9111 <bitgam911@gmail.com>
2023-11-20 16:19:38 -0500

    changes to the main.py structure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import logging
 import sys
 
-import asyncio
-
 from aiohttp import web
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
@@ -30,47 +28,65 @@ async def on_startup(bot: Bot) -> None:
     webhook_info = await bot.get_webhook_info()
     # Set bot commands in UI
     await set_ui_commands(bot)
+    await bot.send_message(chat_id=TELEGRAM_ADMIN_ID, text="Bot is starting")
     print("**************__Initializing the bot__**************\n")
     print("**************__Bot info__**************\n")
-    await bot.send_message(chat_id=TELEGRAM_ADMIN_ID, text="Bot is starting")
     print(bot_info)
     print("\n")
     print(webhook_info)
 
+async def on_shutdown(bot: Bot) -> None:
+    print("**************__Bot is shutting down__**************\n")
+    await bot.session.close()
+
 
 def main() -> None:
+
+    logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",)
+
     engine = create_async_engine(DB_URL, echo=True)
     sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
     
     bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode="HTML")
-    
-    app = web.Application()
-    
+
     # Setup dispatcher
     dp = Dispatcher()
-    dp.startup.register(on_startup)
     dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
     dp.callback_query.middleware(CallbackAnswerMiddleware())
     dp.include_router(commands.router)
-
-    # Create an instance of request handler,
-    # aiogram has few implementations for different cases of usage
-    # In this example we use SimpleRequestHandler which is designed to handle simple cases
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-        secret_token=TELEGRAM_WEBHOOK_SECRET,
-    )
-    webhook_requests_handler.register(app, path="/")
     
-    # Mount dispatcher startup and shutdown hooks to aiohttp application
-    setup_application(app, dp, bot=bot)
+    try:
+        aiohttp_logger = logging.getLogger("aiohttp.access")
+        aiohttp_logger.setLevel(logging.CRITICAL)
+        
+        dp.startup.register(on_startup)
+        dp.shutdown.register(on_shutdown)
+    
+        app = web.Application()
 
-    # And finally start webserver
-    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+        # Create an instance of request handler,
+        # aiogram has few implementations for different cases of usage
+        # In this example we use SimpleRequestHandler which is designed to handle simple cases
+        webhook_requests_handler = SimpleRequestHandler(
+            dispatcher=dp,
+            bot=bot,
+            secret_token=TELEGRAM_WEBHOOK_SECRET,
+        )
+        webhook_requests_handler.register(app, path="/")
+    
+        # Mount dispatcher startup and shutdown hooks to aiohttp application
+        setup_application(app, dp, bot=bot)
+
+        # And finally start webserver
+        web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    
+    except Exception as e:
+        logging.exception(e)
+        sys.exit(1)
     
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     main()
