8f9067d751349a4c0f4e7de2ab9732b815b66ff5
unknown <augustasv1@gmail.com>
2023-12-13 18:51:24 +0200

    launch

--- description.py
+++ description.py
@@ -6,6 +6,6 @@ async def set_description(bot: Bot):
     Sets bot description
     :param bot: Bot instance
     """
-    await bot.set_my_name("JabberBot")
+    await bot.set_my_name("CryptoJab_VIP")
     await bot.set_my_description("This bot is used to purchase subscription to CryptoJab VIP channel.")
     await bot.set_my_short_description("CryptoJab VIP channel subscription bot")
